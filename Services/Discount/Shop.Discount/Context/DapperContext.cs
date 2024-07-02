984579f3c3e8f2911fc6b09ca868fd7a6c3281cf
Batuhan BAŞTÜRK <37836695+bthnbstrk@users.noreply.github.com>
2024-07-02 23:37:42 +0300

    Created discount microservice
    Created discount microservice

---
+++ Services/Discount/Shop.Discount/Context/DapperContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Shop.Discount.Context
+{
+	public class DapperContext : DbContext
+	{
+
+		private readonly IConfiguration _configuration;
+
+		private readonly string _connectionString;
+
+        public DapperContext(IConfiguration configuration)
+		{
+            _configuration = configuration;
+			_connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Servers=;");
+        }
+
+    }
+}
+
